631e8e0e8926a4e4ac4a260d03452ab4d0d18d55
po.lu <lupo.f@outlook.com>
2018-05-11 18:40:02 +0800

    先创建提交，后续补充//TODO

---
+++ wm-applications/auth-application/auth-server/src/main/java/org/github/ycg000344/weiming/authserver/service/AuthService.java
@@ -0,0 +1,23 @@
+/** 
+ * Project Name:auth-server 
+ * File Name:AuthService.java 
+ * Package Name:org.github.ycg000344.weiming.authserver.service 
+ * Date:2018年5月11日下午6:09:22 
+ * Copyright (c) 2018, lupo.f@outlook.com All Rights Reserved. 
+ * 
+*/  
+  
+package org.github.ycg000344.weiming.authserver.service;  
+/** 
+ * ClassName:AuthService <br/><br/>  
+ * Description: 关于外部登录时获取Token <br/><br/>  
+ * Date:     2018年5月11日 下午6:09:22 <br/> <br/> 
+ * @author   po.lu 
+ * @version  1.0.0
+ * @since    JDK 1.8 
+ * @see       
+ */
+public interface AuthService {
+
+}
+  
---
+++ wm-applications/auth-application/auth-server/src/main/java/org/github/ycg000344/weiming/authserver/service/impl/AuthServiceImpl.java
@@ -0,0 +1,26 @@
+/** 
+ * Project Name:auth-server 
+ * File Name:AuthServiceImpl.java 
+ * Package Name:org.github.ycg000344.weiming.authserver.service.impl 
+ * Date:2018年5月11日下午6:09:57 
+ * Copyright (c) 2018, lupo.f@outlook.com All Rights Reserved. 
+ * 
+*/  
+  
+package org.github.ycg000344.weiming.authserver.service.impl;
+
+import org.github.ycg000344.weiming.authserver.service.AuthService;
+
+/** 
+ * ClassName:AuthServiceImpl <br/><br/>  
+ * Description: TODO <br/><br/>  
+ * Date:     2018年5月11日 下午6:09:57 <br/> <br/> 
+ * @author   po.lu 
+ * @version  1.0.0
+ * @since    JDK 1.8 
+ * @see       
+ */
+public class AuthServiceImpl implements AuthService {
+
+}
+  
