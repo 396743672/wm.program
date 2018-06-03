1eb4bd27e3c4ad51fad81fea4392ab04ffacb37a
po.lu <lupo.f@outlook.com>
2018-06-03 21:47:54 +0800

    update

--- wm-applications/dict-application/dict-api/src/main/java/org/github/ycg000344/dictapi/bean/DictionaryObject.java
+++ wm-applications/dict-application/dict-api/src/main/java/org/github/ycg000344/dictapi/bean/DictionaryObject.java
@@ -29,25 +29,109 @@ import lombok.Setter;
 public class DictionaryObject implements Serializable {
 	
     /** 
-	 * serialVersionUID:TODO(用一句话描述这个变量表示什么).
+	 * serialVersionUID.
 	 * 
 	 * @author po.lu
 	 * @version 1.0.0
 	 * @since JDK 1.8
 	 */  
 	private static final long serialVersionUID = -6157015173324684354L;
+	
+	/** 
+	 * dictItemId: 字典项目id.
+	 * 
+	 * @author po.lu
+	 * @version 1.0.0
+	 * @since JDK 1.8
+	 */  
 	private Integer dictItemId;
+    /** 
+     * dictItemKey: 字典项目key.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private String dictItemKey;
+    /** 
+     * dictItemValue: 字典项目value.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Integer dictItemValue;
+    /** 
+     * dictItemCreateTime: 字典项目创建时间.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Date dictItemCreateTime;
+    /** 
+     * dictItemUpdateTime: 字典项目更新时间.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Date dictItemUpdateTime;
+    /** 
+     * dictStatus: 字典项目状态.
+     * 0：禁用；1：可用
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Integer dictItemStatus;
-
+    /** 
+     * dictId: 字典id.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Integer dictId;
+    /** 
+     * dictKey: 字典key.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private String dictKey;
+    /** 
+     * dictValue: 字典value.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Integer dictValue;
+    /** 
+     * dictCreateTime: 字典创建时间.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Date dictCreateTime;
+    /** 
+     * dictUpdateTime: 字典更新时间.
+     * 
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Date dictUpdateTime;
+    /** 
+     * dictStatus: 字典状态.
+     * 0：禁用；1：可用
+     * @author po.lu
+     * @version 1.0.0
+     * @since JDK 1.8
+     */  
     private Integer dictStatus;
     
 
