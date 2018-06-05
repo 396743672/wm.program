ee0106fd5439e3e943e12b581753de48851eda78
po.lu <lupo.f@outlook.com>
2018-06-05 11:27:42 +0800

    spring-boot-admin-demo,没有使用eureka注册中心

---
+++ wm-helloworld/bootadmin-demo/admin-client-demo/src/main/java/org/github/ycg000344/weiming/helloworld/bootadminclient/SpringBootAdminClientApplication.java
@@ -0,0 +1,47 @@
+/** 
+ * Project Name:admin-client-demo 
+ * File Name:SpringBootAdminClientApplication.java 
+ * Package Name:org.github.ycg000344.weiming.helloworld.bootadminclient 
+ * Date:2018年6月5日上午10:53:27 
+ * Copyright (c) 2018, lupo.f@outlook.com All Rights Reserved. 
+ * 
+*/  
+  
+package org.github.ycg000344.weiming.helloworld.bootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/** 
+ * ClassName:SpringBootAdminClientApplication <br/><br/>  
+ * Description: TODO <br/><br/>  
+ * Date:     2018年6月5日 上午10:53:27 <br/> <br/> 
+ * @author   po.lu 
+ * @version  1.0.0
+ * @since    JDK 1.8 
+ * @see       
+ */
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClientApplication.class, args);
+	}
+	
+	@Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()  
+                .and().csrf().disable();
+        }
+    }
+
+}
+  
