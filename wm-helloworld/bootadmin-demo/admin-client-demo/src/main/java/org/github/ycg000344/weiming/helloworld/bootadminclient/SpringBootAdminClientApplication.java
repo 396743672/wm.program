84533060e9736e7f1f1c19ee31de71650c688e99
po.lu <lupo.f@outlook.com>
2018-06-06 17:16:51 +0800

    移除依赖

--- wm-helloworld/bootadmin-demo/admin-client-demo/src/main/java/org/github/ycg000344/weiming/helloworld/bootadminclient/SpringBootAdminClientApplication.java
+++ wm-helloworld/bootadmin-demo/admin-client-demo/src/main/java/org/github/ycg000344/weiming/helloworld/bootadminclient/SpringBootAdminClientApplication.java
@@ -13,8 +13,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /** 
  * ClassName:SpringBootAdminClientApplication <br/><br/>  
@@ -33,15 +31,5 @@ public class SpringBootAdminClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminClientApplication.class, args);
 	}
-	
-	@Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()  
-                .and().csrf().disable();
-        }
-    }
-
 }
   
