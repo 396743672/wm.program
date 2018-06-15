bc389db59c440f69bae5d8899f26df2a31f9afc0
po.lu <lupo.f@outlook.com>
2018-06-15 11:19:12 +0800

    数据字典表结构修改完善

---
+++ wm-applications/application-basicmanager/src/test/java/org/github/ycg000344/weiming/application/basicmanager/BasicManagerAppTest.java
@@ -0,0 +1,34 @@
+/** 
+ * Project Name:application-basicmanager 
+ * File Name:BasicManagerAppTest.java 
+ * Package Name:org.github.ycg000344.weiming.application.basicmanager 
+ * Date:2018年6月15日上午11:10:14 
+ * Copyright (c) 2018, lupo.f@outlook.com All Rights Reserved. 
+ * 
+*/  
+  
+package org.github.ycg000344.weiming.application.basicmanager;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/** 
+ * ClassName:BasicManagerAppTest <br/><br/>  
+ * Description: TODO <br/><br/>  
+ * Date:     2018年6月15日 上午11:10:14 <br/> <br/> 
+ * @author   po.lu 
+ * @version  1.0.0
+ * @since    JDK 1.8 
+ * @see       
+ */
+@SpringBootTest(classes = BasicManagerApp.class)
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+public abstract class BasicManagerAppTest {
+
+
+
+}
+  
