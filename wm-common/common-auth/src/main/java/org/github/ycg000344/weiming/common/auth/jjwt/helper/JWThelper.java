0591a3fa6add50c9f2c5908df49176381da16c21
po.lu <lupo.f@outlook.com>
2018-06-16 16:53:28 +0800

    增加加密方法

--- wm-common/common-auth/src/main/java/org/github/ycg000344/weiming/common/auth/jjwt/helper/JWThelper.java
+++ wm-common/common-auth/src/main/java/org/github/ycg000344/weiming/common/auth/jjwt/helper/JWThelper.java
@@ -49,8 +49,10 @@ public class JWThelper {
 	 * 
 	 * @author po.lu
 	 * @param jwtInfo
-	 * @param priKeyPath 私钥文件路径
-	 * @param expire token有效期(min)
+	 * @param priKeyPath
+	 *            私钥文件路径
+	 * @param expire
+	 *            token有效期(min)
 	 * @return
 	 * @throws InvalidKeySpecException
 	 * 
@@ -96,24 +98,27 @@ public class JWThelper {
 
 	/**
 	 * 
-	 * getInfoFromToken:从公钥文件和token中获取封装的信息. <br/> 
+	 * getInfoFromToken:从公钥文件和token中获取封装的信息. <br/>
 	 * 
 	 * @author po.lu
-	 * @param token 
-	 * @param pubKeyPath 公钥文件路径
+	 * @param token
+	 * @param pubKeyPath
+	 *            公钥文件路径
 	 * @return
-	 * @throws InvalidKeySpecException 
-	 * @throws NoSuchAlgorithmException 
-	 * @throws IllegalArgumentException 
-	 * @throws SignatureException 
-	 * @throws MalformedJwtException 
-	 * @throws UnsupportedJwtException 
-	 * @throws ExpiredJwtException 
-	 * @throws Exception 
-	 * @since JDK 1.8 
+	 * @throws InvalidKeySpecException
+	 * @throws NoSuchAlgorithmException
+	 * @throws IllegalArgumentException
+	 * @throws SignatureException
+	 * @throws MalformedJwtException
+	 * @throws UnsupportedJwtException
+	 * @throws ExpiredJwtException
+	 * @throws Exception
+	 * @since JDK 1.8
 	 * @see
 	 */
-	public static IJWTinfo getInfoFromToken(String token, String pubKeyPath) throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException, IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException {
+	public static IJWTinfo getInfoFromToken(String token, String pubKeyPath)
+			throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException,
+			IllegalArgumentException, NoSuchAlgorithmException, InvalidKeySpecException {
 		Jws<Claims> claimsJws = parserToken(token, pubKeyPath);
 		Claims body = claimsJws.getBody();
 		return new JJWTinfo(body.getSubject(),
@@ -121,50 +126,78 @@ public class JWThelper {
 				StrUtil.nullToDefault((CharSequence) body.get(CommonConstants.JWT_KEY_NAME), ""));
 	}
 
-	/** 
-	 * getInfoFromToken:公钥解析token. <br/> 
-	 * 
-	 * @author po.lu
-	 * @param token 
-	 * @param pubKeyByte 内存中的公钥
-	 * @return 
-	 * @throws NoSuchAlgorithmException 
-	 * @throws InvalidKeySpecException 
-	 * @throws IllegalArgumentException 
-	 * @throws SignatureException 
-	 * @throws MalformedJwtException 
-	 * @throws UnsupportedJwtException 
-	 * @throws ExpiredJwtException 
-	 * @since JDK 1.8 
-	 * @see
-	 */  
-	public static IJWTinfo getInfoFromToken(String token, byte[] pubKeyByte) throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException, IllegalArgumentException, InvalidKeySpecException, NoSuchAlgorithmException {
-		Jws<Claims> claimsJws = parserToken(token, pubKeyByte);
-        Claims body = claimsJws.getBody();
-        return new JJWTinfo(body.getSubject(),
-				StrUtil.nullToDefault((CharSequence) body.get(CommonConstants.JWT_KEY_ID), ""),
-				StrUtil.nullToDefault((CharSequence) body.get(CommonConstants.JWT_KEY_NAME), ""));
-	}
-
-	/** 
-	 * parserToken:公钥解析token. <br/> 
+	/**
+	 * getInfoFromToken:公钥解析token. <br/>
 	 * 
 	 * @author po.lu
 	 * @param token
 	 * @param pubKeyByte
-	 * @return 
-	 * @throws NoSuchAlgorithmException 
+	 *            内存中的公钥
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeySpecException
+	 * @throws IllegalArgumentException
+	 * @throws SignatureException
+	 * @throws MalformedJwtException
+	 * @throws UnsupportedJwtException
+	 * @throws ExpiredJwtException
+	 * @since JDK 1.8
+	 * @see
+	 */
+	public static IJWTinfo getInfoFromToken(String token, byte[] pubKeyByte)
+			throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException,
+			IllegalArgumentException, InvalidKeySpecException, NoSuchAlgorithmException {
+		Jws<Claims> claimsJws = parserToken(token, pubKeyByte);
+		Claims body = claimsJws.getBody();
+		return new JJWTinfo(body.getSubject(),
+				StrUtil.nullToDefault((CharSequence) body.get(CommonConstants.JWT_KEY_ID), ""),
+				StrUtil.nullToDefault((CharSequence) body.get(CommonConstants.JWT_KEY_NAME), ""));
+	}
+
+	/**
+	 * parserToken:公钥解析token. <br/>
+	 * 
+	 * @author po.lu
+	 * @param token
+	 * @param pubKeyByte
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeySpecException
+	 * @throws IllegalArgumentException
+	 * @throws SignatureException
+	 * @throws MalformedJwtException
+	 * @throws UnsupportedJwtException
+	 * @throws ExpiredJwtException
+	 * @since JDK 1.8
+	 * @see
+	 */
+	private static Jws<Claims> parserToken(String token, byte[] pubKeyByte)
+			throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException,
+			IllegalArgumentException, InvalidKeySpecException, NoSuchAlgorithmException {
+		Jws<Claims> claimsJws = Jwts.parser().setSigningKey(RsaKeyHelper.getInstance().getPublicKey(pubKeyByte))
+				.parseClaimsJws(token);
+		return claimsJws;
+	}
+
+	/** 
+	 * generateToken: 密钥加密token. <br/> 
+	 * 
+	 * @author po.lu
+	 * @param jwtInfo
+	 * @param userPriKey
+	 * @param expire
+	 * @return
+	 * @throws NoSuchAlgorithmException
 	 * @throws InvalidKeySpecException 
-	 * @throws IllegalArgumentException 
-	 * @throws SignatureException 
-	 * @throws MalformedJwtException 
-	 * @throws UnsupportedJwtException 
-	 * @throws ExpiredJwtException 
 	 * @since JDK 1.8 
 	 * @see
 	 */  
-	private static Jws<Claims> parserToken(String token, byte[] pubKeyByte) throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException, IllegalArgumentException, InvalidKeySpecException, NoSuchAlgorithmException {
-		Jws<Claims> claimsJws = Jwts.parser().setSigningKey(RsaKeyHelper.getInstance().getPublicKey(pubKeyByte)).parseClaimsJws(token);
-        return claimsJws;
+	public static String generateToken(IJWTinfo jwtInfo, byte[] userPriKey, Integer expire) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		String compactJws = Jwts.builder().setSubject(jwtInfo.getUniqueName())
+				.claim(CommonConstants.JWT_KEY_ID, jwtInfo.getId())
+				.claim(CommonConstants.JWT_KEY_NAME, jwtInfo.getName())
+				.setExpiration(DateTime.now().offset(DateField.MINUTE, expire))
+				.signWith(SignatureAlgorithm.RS256, RsaKeyHelper.getInstance().getPrivateKey(userPriKey)).compact();
+		return compactJws;
 	}
 }
