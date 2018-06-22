6f9fe2220a2a259e875490f4fe1bbe096a4c5c10
po.lu <lupo.f@outlook.com>
2018-06-22 19:01:19 +0800

    查询用户角色路由信息

---
+++ wm-applications/application-basicmanager/src/test/java/org/github/ycg000344/weiming/application/basicmanager/mapper/BaseUserInfoMapperTest.java
@@ -0,0 +1,42 @@
+/** 
+ * Project Name:application-basicmanager 
+ * File Name:BaseUserInfoMapperTest.java 
+ * Package Name:org.github.ycg000344.weiming.application.basicmanager.mapper 
+ * Date:2018年6月22日下午4:42:00 
+ * Copyright (c) 2018, lupo.f@outlook.com All Rights Reserved. 
+ * 
+*/  
+  
+package org.github.ycg000344.weiming.application.basicmanager.mapper;
+
+import org.github.ycg000344.weiming.application.basicmanager.BasicManagerAppTest;
+import org.github.ycg000344.weiming.application.basicmanager.vo.UserInfoVO;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.extern.slf4j.Slf4j;
+
+/** 
+ * ClassName:BaseUserInfoMapperTest <br/><br/>  
+ * Description: TODO <br/><br/>  
+ * Date:     2018年6月22日 下午4:42:00 <br/> <br/> 
+ * @author   po.lu 
+ * @version  1.0.0
+ * @since    JDK 1.8 
+ * @see       
+ */
+@Slf4j
+public class BaseUserInfoMapperTest extends BasicManagerAppTest {
+
+	@Autowired
+	private BaseUserInfoMapper  mapper;
+	
+	@Test
+	public void test() {
+		String userId = "600000";
+		UserInfoVO selectByUserId = mapper.selectByUserId(userId );
+		log.info("***【{}】***",selectByUserId.getRoles().get(0));
+	}
+
+}
+  
