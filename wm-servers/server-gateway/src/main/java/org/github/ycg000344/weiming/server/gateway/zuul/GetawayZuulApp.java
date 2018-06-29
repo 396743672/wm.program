750dfaac789849445ba0586abb0721a16b822fbf
po.lu <lupo.f@outlook.com>
2018-06-29 20:09:59 +0800

    路由网关开启CORS Support

--- wm-servers/server-gateway/src/main/java/org/github/ycg000344/weiming/server/gateway/zuul/GetawayZuulApp.java
+++ wm-servers/server-gateway/src/main/java/org/github/ycg000344/weiming/server/gateway/zuul/GetawayZuulApp.java
@@ -16,6 +16,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /** 
  * ClassName:GetawayZuulApp <br/><br/>  
@@ -35,6 +39,20 @@ public class GetawayZuulApp {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(GetawayZuulApp.class).web(WebApplicationType.SERVLET).run(args);
+	}
+	
+	@Configuration
+	public class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/api/**");
+				}
+			};
+		}
 	}
 
 }
