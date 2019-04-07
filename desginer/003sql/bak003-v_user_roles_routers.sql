16f3f97bb784ef21a5d9e4df291c6eeceaa3f794
po.lu <lupo.f@outlook.com>
2019-04-07 12:13:29 +0800

    docker ing.

--- desginer/003sql/bak003-v_user_roles_routers.sql
+++ desginer/003sql/bak003-v_user_roles_routers.sql
@@ -1,4 +1,4 @@
-CREATE VIEW `v_user_roles_routers` AS SELECT
+SELECT
 	t3.user_id AS user_id,
 	t3.login_name AS login_name,
 	t3.user_name AS user_name,
@@ -7,7 +7,7 @@ CREATE VIEW `v_user_roles_routers` AS SELECT
 	t3.status AS status,
 	IFNULL(t4.role_ids,'') AS role_ids,
 	IFNULL((SELECT group_concat( t1.role_name SEPARATOR ',' ) FROM t_base_role t1 WHERE FIND_IN_SET(t1.role_id,t4.role_ids)),'') AS roles, -- 将角色名称使用,进行连接
-	IFNULL(t3.router_ids,'') AS router_ids
+	IFNULL(t3.router_ids,'') AS router_ids 
 FROM
 	(
 	(
@@ -23,11 +23,11 @@ SELECT
 FROM
 	(
 	t_base_user t1
-	LEFT JOIN t_base_user_routers t2 ON ( ( ( t1.user_id = t2.user_id ) AND ( t2.status = 1 ) ) )
-	)
+	LEFT JOIN t_base_user_routers t2 ON ( ( ( t1.user_id = t2.user_id ) AND ( t2.status = 1 ) ) ) 
+	) 
 GROUP BY
-	t1.user_id
-	)
+	t1.user_id 
+	) 
 	) t3
-	LEFT JOIN t_base_user_roles t4 ON ( ( ( t3.user_id = t4.user_id ) AND ( t4.status = 1 ) ) )
+	LEFT JOIN t_base_user_roles t4 ON ( ( ( t3.user_id = t4.user_id ) AND ( t4.status = 1 ) ) ) 
 	)
